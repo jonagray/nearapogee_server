c32aaffd3c76a3111933183d3164ce306d05f95b
Jonny Graybill <jon.a.graybill@gmail.com>
2020-05-18 14:11:29 -0700

    all server files

---
+++ database.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE nearapogee_auth;
+
+-- Create table for login/logout users
+CREATE TABLE nearapogee_users(
+  user_id uuid PRIMARY KEY DEFAULT
+  uuid_generate_v4(),
+  nearapogee_user_name VARCHAR(255) NOT NULL,
+  nearapogee_user_email VARCHAR(255) NOT NULL,
+  nearapogee_user_password VARCHAR(255) NOT NULL
+);
+
+-- Create table for consumer's database
+CREATE TABLE users(
+  user_id SERIAL PRIMARY KEY,
+  name VARCHAR(30),
+  email VARCHAR(30),
+  address VARCHAR(255)
+);
